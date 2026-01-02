d13974f0d9c11c609fa9c1f3d2042cf5944a5ff1
Thanh Trinh <thanhnghiep1007@gmail.com>
2026-01-02 12:51:45 -0800

    fix path

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const initSqlJs = require('sql.js');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const uploadsDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir);
